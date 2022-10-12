9f241e59e067414137b91886a571918d44a80097
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-12 17:08:39 -0400

    put in code

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Created by: Allen
+// Created on: Oct 12
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+ function enterClicked() {
+    // input
+    const StreetNumber = document.getElementById("Street-Number").value
+    const StreetName = document.getElementById("Street-Name").value
+  
+    // output
+    document.getElementById("address").innerHTML =
+      "Your address is: " + StreetNumber + " " + StreetName + " Longfields."
+  }
+  
