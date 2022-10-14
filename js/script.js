58f0999b87b905aeb0ab712ce80412eb1179975c
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-14 14:27:28 -0400

    input

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
  */
 function enterClicked() {
   // input
-  const streetNumber = document.getElementById("Street-Number").value
-  const streetName = document.getElementById("Street-Name").value
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
 
   // output
   document.getElementById("address").innerHTML =
